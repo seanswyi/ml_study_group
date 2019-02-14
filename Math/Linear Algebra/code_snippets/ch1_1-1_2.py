519af1f445cc62e20a8e3812e3689c5ecf9ba13c
Sean Yi / 이석원 <seantuokc@gmail.com>
2019-02-14 22:08:49 +0900

    Added Math directory.

---
+++ Math/Linear Algebra/code_snippets/ch1_1-1_2.py
@@ -0,0 +1,38 @@
+'''
+An Introduction to Linear Algebra (4e) - Gilbert Strang
+
+Chapter 1
+Problem Set 1-1
+#2
+'''
+
+import matplotlib.pyplot as plt
+
+
+# v
+plt.quiver(0, 0, 4, 1, color='C0', angles='xy', scale_units='xy', scale=1, label=r'$\vec{v}$')
+
+# w
+plt.quiver(0, 0, -2, 2, color='C1', angles='xy', scale_units='xy', scale=1, label=r'$\vec{w}$')
+
+# -w
+plt.quiver(0, 0, 2, -2, color='C2', angles='xy', scale_units='xy', scale=1, label=r'$\vec{-w}$')
+
+# v + w
+plt.quiver(0, 0, 2, 3, color='C3', angles='xy', scale_units='xy', scale=1, label=r'$\vec{v} + \vec{w}$')
+
+# v - w
+plt.quiver(0, 0, 6, -1, color='C4', angles='xy', scale_units='xy', scale=1, label=r'$\vec{v} - \vec{w}$')
+
+# Dotted lines.
+plt.plot([-2, 2], [2, 3], 'C0--')
+plt.plot([2, 4], [3, 1], 'C1--')
+plt.plot([2, 6], [-2, -1], 'C0--')
+plt.plot([4, 6], [1, -1], 'C2--')
+
+# Miscellaneous.
+plt.xlim(-4, 7)
+plt.ylim(-4, 7)
+plt.grid()
+plt.legend()
+plt.show()
